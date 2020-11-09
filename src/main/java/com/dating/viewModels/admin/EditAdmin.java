3c9eaba57998e914c572e6ce01e2700639dec604
Vibs <vibej@hotmail.com>
2020-11-09 19:19:03 +0100

    - EditAdmin-klasse tilføjet til viewModels - PreviewDatingUser-klasse tilføjet til viewModels

---
+++ src/main/java/com/dating/viewModels/admin/EditAdmin.java
@@ -0,0 +1,12 @@
+package com.dating.viewModels.admin;
+
+public class EditAdmin
+{
+    private String username;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    
+    
+    
+}
---
+++ src/main/java/com/dating/viewModels/datingUser/PreviewDatingUser.java
@@ -0,0 +1,10 @@
+package com.dating.viewModels.datingUser;
+
+public class PreviewDatingUser
+{
+    private String imagePath;
+    private String username;
+    private int age;
+    
+    
+}
