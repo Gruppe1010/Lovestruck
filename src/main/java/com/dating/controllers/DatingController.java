b31fb4fa9aad812b0a8ef533c5e87d856c2a540c
RRoager <rasmus-roager@hotmail.com>
2020-11-11 09:50:06 +0100

    Postnummer besked er ændret

--- src/main/java/com/dating/controllers/DatingController.java
+++ src/main/java/com/dating/controllers/DatingController.java
@@ -352,8 +352,6 @@ public class DatingController
         
        og så thymeleafer vi en array på favouritespage-html'en
          */
-        
-        
         return "Admin/blacklisteduserspageadmin"; // html
     }
     
